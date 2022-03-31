1f351c4f23b5e37b37a3166b9e13b51c637da08e
palikilaGayatri <102041802+palikilaGayatri@users.noreply.github.com>
2022-03-31 14:24:21 +0530

    initial commit - moving project

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter a number");
+    scanf("%d",&a);
+    printf("Enter a number");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum of the two numbers is %d",c);
+    return 0;
+}
+}
